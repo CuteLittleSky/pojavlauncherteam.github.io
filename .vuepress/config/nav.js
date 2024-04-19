a9b8935f40d549e305c728f6d11c43dea4674bbf
CuteLittleSky <62592731+CuteLittleSky@users.noreply.github.com>
2024-04-20 00:38:39 +0800

    Create nav.js
    Signed-off-by: CuteLittleSky <62592731+CuteLittleSky@users.noreply.github.com>

---
+++ .vuepress/config/nav.js
@@ -0,0 +1,19 @@
+module.exports = [
+  // Nav 1
+  {
+    text: "首页",
+    link: "/"
+  },
+  {
+    text: "我的世界导航",
+    link: "/mc/"
+  },
+  {
+    text: "SKYCRAFT服务器IP:MC.SKYCRAFT.CN",
+    link: "https://skycraft.cn"
+  },
+  {
+    text: "加入SKYCRAFT服务器QQ群",
+    link: "https://jq.skycraft.cn/"
+  }
+];
